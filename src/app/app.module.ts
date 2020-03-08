f08fb3f27c48a26a46f23b952aae7f8c18b46947
Mensah A <attmensah@gmail.com>
2020-03-08 13:46:24 -0400

    udapte apollo-10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//  {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+// import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
