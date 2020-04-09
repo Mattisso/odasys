06fe3a0af92b88e64823c2c76060731e418a8fa8
Mensah A <attmensah@gmail.com>
2020-04-08 21:58:01 -0400

    update package

--- src/server/testing/oexerccompta.js
+++ src/server/testing/oexerccompta.js
@@ -13,7 +13,8 @@ const {update$, delete$, index$$,insert$$ } = require('../features/oexerccompta/
 
 const {toseedoexercompta} = require('../features/oexerccompta/oexercomptaSeed').toinit();
 
-const getoreportdetail$ =  update$(objoexerccomptadata,'5e8b7a140310040e3c00481d')
+const getoreportdetail$ =index$$;
+//update$(objoexerccomptadata,'5e8b7a140310040e3c00481d')
 // getstreamdata$(getoreportdetail$(objoexerccomptadata())).subscribe(odagetObserver());
 
 // console.log((toseedoexercompta));
@@ -23,7 +24,7 @@ const getoreportdetail$ =  update$(objoexerccomptadata,'5e8b7a140310040e3c00481d
 // delete$('5de4209ff5f8b20fe8229f97');5ded92811d2b1a18540e9e41
 
 //update$(objupdate,'5dbac506aa1fc24648be97d1');
-///delete$('5e0ff441a04861607811d19a') 
+///delete$('5e0ff441a04861607811d19a')
 //insert$$(obj); //DropDownListexerComptable$ //getByid$ '5dbb6c97c2d9871f78c16a2a');
 
 getstreamdata$(getoreportdetail$).subscribe(odagetObserver());
